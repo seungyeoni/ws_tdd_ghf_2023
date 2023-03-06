d344450da7afaa82c7503a4f440ab12926a19299
seungyeoni <dnri33@naver.com>
2023-03-06 15:43:33 +0900

    테스트케이스, 출력을 모니터에 하지 않고 문자열로 얻기

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -1,19 +1,34 @@
 package com.ll;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 public class AppTests {
     @Test
     @DisplayName("스캐너에 키보드가 아닌 문자열을 입력으로 설정")
     public void t1() {
         Scanner sc = TestUtil.genScanner("안녕");
-
         String cmd = sc.nextLine().trim();
         assertThat(cmd).isEqualTo("안녕");
+    }
+
+    @Test
+    @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
+    public void t2() {
+        // System.out 에 대한 화면출력 금지 시작
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        System.out.print("안녕");
+
+        // 그 동안 출력되지 않던 내용들을 문자열로 반환
+        String rs = output.toString();
+
+        // System.out 에 대한 화면출력 금지 끝
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs).isEqualTo("안녕");
     }
 }
--- src/test/java/com/ll/TestUtil.java
+++ src/test/java/com/ll/TestUtil.java
@@ -2,6 +2,7 @@ package com.ll;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
@@ -10,5 +11,21 @@ public class TestUtil {
         InputStream in = new ByteArrayInputStream(input.getBytes());
 
         return new Scanner(in);
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
