8023a2c111a431d5489c5a6b4556821ad8de8919
seungyeoni <55572263+seungyeoni@users.noreply.github.com>
2023-03-06 21:26:54 +0900

    Enhancement/3 (#4)
    * 명언 등록시 마다 번호 증가
    
    * 명언 등록시 마다 번호 증가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,28 +1,34 @@
 package com.ll;
-
 import java.util.Scanner;
-
 public class App {
     private final Scanner sc;
-
     public App(Scanner sc) {
         this.sc = sc;
     }
-
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        long lastWiseSayingId = 0;
 
         while (true) {
             System.out.print("명령) ");
 
             String cmd = sc.nextLine().trim();
-
             if (cmd.isEmpty()) continue;
-
             switch (cmd) {
                 case "종료":
                     System.out.println("프로그램이 종료되었습니다.");
                     return;
+                case "등록":
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.print("작가 : ");
+                    String authorName = sc.nextLine().trim();
+
+                    long id = ++lastWiseSayingId;
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
                 default:
                     System.out.printf("`%s`(은)는 올바르지 않은 명령입니다.\n", cmd);
                     break;
